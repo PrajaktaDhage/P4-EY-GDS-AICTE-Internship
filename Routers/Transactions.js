2ec89066ee95398e7c9be8192fe595d042a79510
PrajaktaDhage <153431374+PrajaktaDhage@users.noreply.github.com>
2025-03-03 22:11:52 +0530

    Transactions.js

--- Routers/Transactions.js
+++ Routers/Transactions.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import { addTransactionController, deleteTransactionController, getAllTransactionController, updateTransactionController } from '../controllers/transactionController.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = express.Router();
 
-router.route("/addTransaction").post(addTransactionController);
-
-router.route("/getTransaction").post(getAllTransactionController);
-
-router.route("/deleteTransaction/:id").post(deleteTransactionController);
-
-router.route('/updateTransaction/:id').put(updateTransactionController);
+router.route("/addTransaction").post(authenticate, addTransactionController);
+router.route("/getTransaction").post(authenticate, getAllTransactionController);
+router.route("/deleteTransaction/:id").post(authenticate, deleteTransactionController);
+router.route('/updateTransaction/:id').put(authenticate, updateTransactionController);
 
 export default router;
