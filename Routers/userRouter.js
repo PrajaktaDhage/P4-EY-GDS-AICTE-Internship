9142c58a041374f4092a81d528ed65efc61acee5
PrajaktaDhage <153431374+PrajaktaDhage@users.noreply.github.com>
2025-03-03 22:12:42 +0530

    userRouter.js

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import { loginControllers, registerControllers, setAvatarController } from '../controllers/userController.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = express.Router();
 
 router.route("/register").post(registerControllers);
-
 router.route("/login").post(loginControllers);
-
-router.route("/setAvatar/:id").post(setAvatarController);
+router.route("/setAvatar/:id").post(authenticate, setAvatarController);
 
 export default router;
