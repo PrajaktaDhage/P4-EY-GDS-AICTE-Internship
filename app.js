76ccbb060db180f98da6e929c78781173610309d
PrajaktaDhage <153431374+PrajaktaDhage@users.noreply.github.com>
2025-03-03 22:13:16 +0530

    app.js

--- app.js
+++ app.js
@@ -4,26 +4,31 @@ import bodyParser from "body-parser";
 import { connectDB } from "./DB/Database.js";
 import transactionRoutes from "./Routers/Transactions.js";
 import userRoutes from "./Routers/userRouter.js";
+import dotenv from "dotenv";
+import morgan from "morgan";
 
-//app is express server
+dotenv.config();
+
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
-//database connection triggered
 connectDB();
-//we are registering cors - Cross Origin Resource Sharing - This allow our server to respond to our frontend requests
 app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
-// Middleware - is a logic which executes before the backend
-app.use(express.json());//{key:value}
-app.use(bodyParser.urlencoded({ extended: false }));//name=hmg
-
-// Adding 2 routes
-app.use("/api/v1", transactionRoutes);//all end points related to credit/debit transactions
-app.use("/api/auth", userRoutes);//this all end points related to users -> login, signup
+app.use("/api/v1", transactionRoutes);
+app.use("/api/auth", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Finance Manager Server is working");
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ success: false, message: "Something went wrong!" });
 });
 
 app.listen(port, () => {
