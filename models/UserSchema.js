adf9c81c948086c8f04161ae245bab1d0c1dcc2c
PrajaktaDhage <153431374+PrajaktaDhage@users.noreply.github.com>
2025-03-03 22:03:16 +0530

    UserSchema.js

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -2,23 +2,25 @@ import mongoose from "mongoose";
 import validator from "validator";
 
 // User Schema Model - (Name, email, password, creation Date) with validation rules
-//new mongoose.Schema({feild1:{type:dtype,contraint:value},feild2:{type:dtype,contraint:value}})
-//CREATE TABLE USER(name varchar(25) notnull)
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, "Name is required"],
+        trim: true,
     },
     email: {
         type: String,
         required: [true, "Email is required"],
-        unique : true,
-        validate : validator.isEmail,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: "Please provide a valid email",
+        },
     },
     password: {
         type: String,
         required: [true, "Password is required"],
-        minlength : [6, "Password Must Be Atleast 6 characters"],
+        minlength: [6, "Password must be at least 6 characters"],
     },
     isAvatarImageSet: {
         type: Boolean,
@@ -26,16 +28,15 @@ const userSchema = new mongoose.Schema({
     },
     avatarImage: {
         type: String,
-        default: ""
+        default: "",
     },
-    transactions: {
-        type: [],
-    },
-    createdAt: {
-        type:Date,
-        default: Date.now,
-    },
-});
+    transactions: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Transaction',
+    }],
+}, { timestamps: true });
+
+userSchema.index({ email: 1 }); // Ensure email is indexed
 
 const User = mongoose.model("User", userSchema);
 
